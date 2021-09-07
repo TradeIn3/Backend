583f776400840690878afa4a4bd6dc3b179bd2d6
Rohit0301 <rj03012002@gmail.com>
2021-09-07 19:06:14 +0530

    change settings

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -136,7 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ORIGIN_WHITELIST = (
   'http://localhost:8000',
 )
